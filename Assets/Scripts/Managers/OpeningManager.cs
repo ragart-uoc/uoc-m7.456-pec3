b611270595fd59938b64d19183be017e3e7d2b8e
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2022-12-21 09:14:18 +0100

    Created opening scene

---
+++ Assets/Scripts/Managers/OpeningManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+namespace PEC3.Managers
+{
+    public class OpeningManager : MonoBehaviour
+    {
+        public TextMeshProUGUI screenText;
+
+        private IEnumerator Start()
+        {
+            screenText.canvasRenderer.SetAlpha(0.0f);
+            screenText.text = "Salvador Banderas presents";
+            screenText.CrossFadeAlpha(1.0f, 1.5f, false);
+            yield return new WaitForSeconds(2.5f);
+            screenText.CrossFadeAlpha(0.0f, 1.5f, false);
+            yield return new WaitForSeconds(1.5f);
+
+            Application.Quit();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    }
+}
